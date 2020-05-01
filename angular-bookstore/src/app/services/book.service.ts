de067dad6f0633f54bbf7211e1db1f584ebfae14
Juan Mario <jmarioguevara@gmail.com>
2020-05-01 12:37:04 -0400

    f6

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Book } from '../common/book';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Book } from '../common/book';
 import { BookCategory } from '../common/book-category';
 
 @Injectable({
@@ -16,42 +16,41 @@ export class BookService {
   constructor(private httpClient: HttpClient) { }
 
   getBooks(theCategoryId: number): Observable<Book[]>{
-
-    //const searchUrl = '${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}';
     const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
-
-    return this.getBooksList(searchUrl)
-  }
-
-  private getBooksList(searchUrl: string): Observable<Book[]> {
-    return this.httpClient.get<GetResponseBooks>(searchUrl).pipe(map(response => response._embedded.books));
+    return this.getBooksList(searchUrl);
   }
 
   getBookCategories(): Observable<BookCategory[]>{
-    
     return this.httpClient.get<GetResponseBookCategory>(this.categoryUrl).pipe(
-      map(response => response._embedded.bookCategory)
+      map(response => response._embedded.bookCateogry)
     );
-
   }
 
-
-  searchBooks(keyword: string): Observable<Book[]>{    
+  searchBooks(keyword: string): Observable<Book[]>{
     const searchUrl = `${this.baseUrl}/search/searchbykeyword?name=${keyword}`;
-    return this.getBooksList(searchUrl)
+    return this.getBooksList(searchUrl);
   }
 
+  private getBooksList(searchUrl: string): Observable<Book[]> {
+    return this.httpClient.get<GetResponseBooks>(searchUrl).pipe(
+      map(response => response._embedded.books)
+    );
+  }
 
+  get(bookId: number): Observable<Book> {
+    const bookDetailsUrl = `${this.baseUrl}/${bookId}`;
+    return this.httpClient.get<Book>(bookDetailsUrl);
+  }
 }
 
 interface GetResponseBooks{
-  _embedded:{
+  _embedded: {
     books: Book[];
   }
 }
 
 interface GetResponseBookCategory{
-  _embedded:{
-    bookCategory: BookCategory[];
+  _embedded: {
+    bookCateogry: BookCategory[];
   }
 }
